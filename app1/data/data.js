ce423a0f8f21a959a4c1b8edfbb7f6fe6b3494fa
liyi199488 <443999337@qq.com>
2018-06-11 19:09:33 +0800

    5.2
    数据从业务中分享；小程序模块

---
+++ app1/data/data.js
@@ -0,0 +1,36 @@
+var postList=[
+{
+  date: "2017年2月2日",
+  title: "小时候的冰棍和雪糕",
+    postImg: '/image/post/post-4.jpg',
+      avatar: '/image/avatar/avatar-5.png',
+        content: '冰棍与雪糕绝对不是同一个东西，3到5毛钱的雪糕犹如现在的哈哈根达斯，而5分到1毛 的冰杨棍儿就像现在的老冰棒，时过境迁，……',
+          readingNum: 92,
+            collectNum: 100,
+              commentNum: 7
+},
+{
+  date: "2017年2月2日",
+  title: "小时候的冰棍和雪糕",
+    postImg: '/image/post/post-1.jpg',
+      avatar: '/image/avatar/avatar-3.png',
+        content: '年少时，有几样东西，是春节里必不可少的：烟花、新衣、凉菜、压岁钱、饺子、年分大小年，有的地方是腊月二十三过小年，有的地方是腊月二十四……',
+          readingNum: 92,
+            collectNum: 100,
+             commentNum: 7
+},
+{
+  date: "2017年2月2日",
+  title: "从童年呼啸面过的火车",
+    postImg: '/image/post/post-5.jpg',
+      avatar: '/image/avatar/avatar-1.png',
+        content: '小时候，家的后面有一条铁路，听説从南方北上的火车都必须经过这条铁路，坐车大多在晚上经过，但了不定是只有在夜深人静的时候，火车的声音才能从远方传过来，……',
+          readingNum: 92,
+            collectNum: 100,
+             commentNum: 7
+}
+];
+
+module.exports={
+  postList:postList
+};
